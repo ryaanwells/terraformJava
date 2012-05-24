479e2ecb05bbc6847c4dabf7d0206311c9baa7a1
Ryan Wells <ryaanwells@googlemail.com>
2012-05-24 19:50:00 +0100

    dual layer on map added

--- src/map.java
+++ src/map.java
@@ -2,40 +2,44 @@ import java.util.Random;
 import java.lang.StringBuilder;
 
 public class map{
-    private int[][] terrain;
+    private int[][][] terrain;
     private int size = 10;
+    
     map(){
 	Random randomGenerator = new Random();
-	terrain = new int[size][size];
+	terrain = new int[2][size][size];
 	for(int i=0; i<size; i++){
 	    for(int j=0; j<size; j++){
-		terrain[i][j] =(int) randomGenerator.nextInt(5);
+		terrain[0][i][j] =(int) randomGenerator.nextInt(5);
+		terrain[1][i][j] = 1;
 	    }
 	}
     }
     map(String p){
-	terrain = new int[size][size];
+	terrain = new int[2][size][size];
 	for(int i=0; i<size; i++){
 	    for(int j=0; j<size; j++){
-		terrain[i][j] = 1;
+		terrain[0][i][j] = 1;
+		terrain[1][i][j] = 1;
 	    }
 	}
     }
     map(int n){
 	size = n;
-	terrain = new int[n][n];
+	terrain = new int[2][n][n];
 	for(int i=0; i<n; i++){
 	    for(int j=0; j<n; j++){
-		terrain[i][j]=0;
+		terrain[0][i][j]=1;
+		terrain[1][i][j]=1;
 	    }
 	}
     }		
 
-    public int get(int x, int y){
-	return terrain[x][y];
+    public int get(int l, int x, int y){
+	return terrain[l][x][y];
     }
-    public void set(int x, int y, int n){
-	terrain[x][y] = n;
+    public void set(int l, int x, int y, int n){
+	terrain[l][x][y] = n;
     }
 
     public String toString(){
@@ -44,7 +48,7 @@ public class map{
 	for(int i = 0; i<size; i++){
 	    output.append("[ ");
 	    for(int j=0; j<size; j++){
-		temp = get(i,j);
+		temp = terrain[0][i][j];
 		if(temp==0){output.append("#");}
 		if(temp!=0){output.append("+");}
 		output.append(" ");
--- src/movement.java
+++ src/movement.java
@@ -8,7 +8,7 @@ public class movement{
     private int offsetY;
     //variables easily changed for actual units
     private int enemy = 1;
-    private int 
+    private int bonus = 2;
 
     movement(map M, unit U){
 	workingMap = M;
@@ -21,21 +21,17 @@ public class movement{
 
     private boolean canAccess(int x, int y, int movement){
 	if(x<0 || y<0) return false;
-	return ((workingMap.get(x,y)!=0)&&(movement-1>=0));
+	return ((workingMap.get(0,x,y)!=0)&&(movement-1>=0));
     }
-
-    private boolean isEnemy(int x, int y){
-	return foo;}
 
     private void probeNext(int prevx, int prevy, int x, int y, int movement){
 	if(movement >= 0){
-	    canMove.set(x-offsetX,y-offsetY,1);
+	    canMove.set(0,x-offsetX,y-offsetY,0);
 	    if(y!=prevy+1 && canAccess(x,y-1,movement)){probeNext(x,y,x,y-1,movement-1);}
 	    if(y!=prevy-1 && canAccess(x,y+1,movement)){probeNext(x,y,x,y+1,movement-1);}
 	    if(x!=prevx+1 && canAccess(x-1,y,movement)){probeNext(x,y,x-1,y,movement-1);}
 	    if(x!=prevx-1 && canAccess(x+1,y,movement)){probeNext(x,y,x+1,y,movement-1);}
 	}
-	
     }
 
     public String toString(){
@@ -43,7 +39,7 @@ public class movement{
     }
 
     public void processAccessable(){
-	if(canMove!=null){	
+	if(canMove!=null){
 	    int movement = workingUnit.getMovement();
 	    int startX = workingUnit.getX();
 	    int startY = workingUnit.getY();
